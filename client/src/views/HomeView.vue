f01b7bc3f20a6d705f0e54055541e3853903a76d
raymcnd <89778453+raymcnd@users.noreply.github.com>
2023-08-16 05:15:42 +0700

    feat: add more info to home page

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -16,10 +16,16 @@ export default {
 </script>
 
 <template>
-  <section style="width: 95vw;  height: 95vh; display: flex; flex-direction: column; justify-content: center; align-items: center;">
-      <h2 class="fw-bold text-white text-left" style="width: 600px;">Welcome</h2>
-      <p class="fw-semibold text-white text-left" style="width: 600px;">
-        <button class="btn mt-3 fw-semibold text-white" style="background-color: rgb(2, 211, 9);" @click="demoButton">Demo</button>
+  <section style="width: 100vw;  height: 100vh; display: flex; flex-direction: column; justify-content: center; align-items: center;">
+      <h2 class="fw-bold text-white text-left mb-4" style="width: 400px;">Welcome</h2>
+      <p class="text-white" style="width: 400px;"><b>isAvailable</b> is a versatile reservation app that can handle reservations based on product type and session.<br><br>
+        It can be useful for businesses that rely on reservation, such as private tutors, personal trainers, models, photographers, sport arenas, music studios, and so on.</p>
+      <p class="fw-semibold text-white text-left" style="width: 400px;">
+        <button class="btn mt-3 fw-semibold text-white" style="background-color: rgb(2, 211, 9); width: 400px;" @click="demoButton">Demo</button>
+        <a class="nav-link" href="https://github.com/raymcnd/isAvailable" target="_blank"><button class="mt-3 btn fw-semibold text-white d-flex align-items-center justify-content-center" style="background-color: rgb(2, 211, 9); width: 400px;">
+          <img src="../assets/githublogo.png" class="me-2" style="width: 20px;" />  
+          Github Repository
+        </button></a>
       </p>
   </section>     
 </template>
--- client/src/views/ReservationView.vue
+++ client/src/views/ReservationView.vue
@@ -56,18 +56,26 @@ export default {
   },
   watch: {
     productSelected() {
+      try {
         for (let product of this.availabilityByDate) {
             if (product.productId === this.productSelected.id) {
                 this.availableSessions = product.availableSessions
             }
         }
+      } catch (error) {
+        console.log(error)
+      }
     },
     date() {
+      try {
         for (let product of this.availabilityByDate) {
             if (product.productId === this.productSelected.id) {
                 this.availableSessions = product.availableSessions
             }
         }
+      } catch (error) {
+        console.log(error)
+      }
     }
   }
 }
@@ -75,15 +83,17 @@ export default {
 
 <template>
   <section class="mt-5" style="width: 95vw;  height: 95vh; display: flex; flex-direction: column; align-items: center;">
-      <h1 class="fw-bold text-white text-left ps-5" style="width: 800px;">Is Beetle Music<br>available?</h1>
-      <div class="mt-4" style="display: flex; width: 800px;gap: 20px;">
-        <div class="d-flex justify-content-center align-item-center p-5">
+      <h1 class="fw-bold text-white text-left ps-5 text-center mb-5" style="width: 800px;">Is <u>Beetle Music</u> available?</h1>
+      <div class="mt-4" style="display: flex; width: 800px;gap: 20px">
+        <div class="d-flex justify-content-center align-item-center p-5" style="flex-direction: column;">
+          <h5 class="text-white mb-3">Pick a Date 📅</h5>
           <VueDatePicker v-model="date" inline auto-apply :enable-time-picker="false" :min-date="new Date()" @update-month-year="handleMonthYear"/>
         </div>
       
         <div class="card py-3 px-4 text-white rounded-right" style="background-color: blueviolet; flex-grow: 1;" v-if="card === 'availability'">
-            
-            <h5>Available Products</h5>
+          <h5 class="mb-3">Availability 🕒</h5>
+          
+          <h6>Choose a Product</h6>
           <p>
             <div class="form-check" v-for="product in availabilityByDate" :key="product.productId">
               <input class="form-check-input" type="radio" :value="{id: product.productId, name: product.productName, price: product.productPrice}" v-model="productSelected">
@@ -91,7 +101,7 @@ export default {
             </div>
           </p>
 
-          <h5 class="mt-4">Available Sessions</h5>
+          <h6 class="mt-4">Choose a Session</h6>
           <div>
             <div class="form-check" v-for="session in availableSessions" :key="session.sessionId">
               <input class="form-check-input" type="radio" :value="{id: session.sessionId, name: session.sessionName}" v-model="sessionSelected">
@@ -103,11 +113,11 @@ export default {
           </div>
 
           <div style="flex-grow: 1;"></div>
-          <button class="card-footer btn fw-bold shadow mb-2 rounded" style="background-color: rgb(2, 211, 9);" @click="nextButton">Next</button>
+          <button class="card-footer btn fw-bold mb-2 rounded" style="background-color: rgb(2, 211, 9);" @click="nextButton">Next</button>
         </div>
 
         <div class="card py-3 px-4 text-white rounded-right" style="background-color: blueviolet; flex-grow: 1;" v-if="card === 'reservationDetails'">
-            <h5 class="mb-2">Reservation Details</h5>
+            <h5 class="mb-2">Reservation Details 🧾</h5>
             <form style="display: flex; flex-direction: column;" @submit.prevent="payment(productSelected.id)">
                 <div class="mb-2">
                     <label for="" class="form-label">Date</label>
@@ -131,7 +141,7 @@ export default {
                 </div> -->
 
                 <div class="d-flex justify-content-end mt-2">
-                    <button class="btn btn-warning fw-bold text-white" @click="backButton">Back</button>
+                    <button class="btn fw-bold text-white" @click="backButton" style="background-color: rgb(242, 191, 38);">Back</button>
                     <button type="submit" class="btn ms-3 fw-bold text-white" style="background-color: rgb(2, 211, 9);">Submit</button>
                 </div>
             </form>
